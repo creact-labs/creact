7a05587ff3a8d174329574705fb7552ae202cc7c
Daniel <dcoutinho.96@gmail.com>
2025-09-28 21:51:40 -0300

    fix path resolve in deploy.go

--- go/deploy.go
+++ go/deploy.go
@@ -43,7 +43,7 @@ func main() {
 	fail := color.New(color.FgRed).SprintFunc()
 
 	// Load shared config
-	sharedFile := filepath.Join("..", "config", "shared", "shared.json")
+	sharedFile := "./config/shared/shared.json"
 	data, err := os.ReadFile(sharedFile)
 	if err != nil {
 		log.Fatalf("Failed to read shared config: %v", err)
